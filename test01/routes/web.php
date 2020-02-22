f22bf10b8cbc18235c4268ac16c5f7639a4458dc
yi-fan Lin <36557630+lamoo990069@users.noreply.github.com>
2020-02-22 19:33:05 +0800

    npm

--- test01/routes/web.php
+++ test01/routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
+use Illuminate\Routing\Route;
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/posts', function () {
+    return view('posts.list');
+});
+Route::get('/posts/{id}', function ($id) {
+    return view('posts.show');
+});
+
