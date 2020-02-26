a892ba6fefc3104414ce4f1647b21889cfebd565
yi-fan Lin <36557630+lamoo990069@users.noreply.github.com>
2020-02-26 21:17:34 +0800

    5.8_header_request_nav-active

--- test01/routes/web.php
+++ test01/routes/web.php
@@ -13,6 +13,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('index');
 });
@@ -30,3 +31,4 @@ Route::get('/posts/{id}', function ($id) {
     return view('posts.show');
 });
 
+?>
