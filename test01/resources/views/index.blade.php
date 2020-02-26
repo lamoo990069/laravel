090c8e0140696bd57c88f6047036dc3dd28fb7c4
yi-fan Lin <36557630+lamoo990069@users.noreply.github.com>
2020-02-26 15:03:53 +0800

    5.1

--- test01/resources/views/index.blade.php
+++ test01/resources/views/index.blade.php
@@ -1,9 +1,72 @@
 @extends('layouts.app')
 
 @section('hero')
-hero
+<div class="banner-state vertical-align text-center banner-13">
+    <div class="container-mid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="banner-title">
+                        <h1 class="text-uppercase light-txt m-bot-10 inline-block">think wiser</h1>
+                        <h3 class="text-uppercase light-txt m-bot-30 inline-block">we are massive for general </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('content')
-content
+<div class="page-content">
+    <div class="container">
+        <div class="row ">
+
+            <div class="heading-title text-center">
+                <h3 class="text-uppercase"> We believe to design the impossible</h3>
+                <p class="half-txt p-top-30">Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.</p>
+            </div>
+
+            <div class="col-md-4">
+                <div class="featured-item feature-bg-box gray-bg text-center m-bot-0 inline-block radius-less">
+                    <div class="icon">
+                        <i class="icon-mobile"></i>
+                    </div>
+                    <div class="title text-uppercase">
+                        <h4>Fully responsive</h4>
+                    </div>
+                    <div class="desc">
+                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="featured-item feature-bg-box gray-bg text-center m-bot-0 inline-block radius-less">
+                    <div class="icon">
+                        <i class="icon-layers"></i>
+                    </div>
+                    <div class="title text-uppercase">
+                        <h4>premium sliders</h4>
+                    </div>
+                    <div class="desc">
+                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="featured-item feature-bg-box gray-bg text-center m-bot-0 inline-block radius-less">
+                    <div class="icon">
+                        <i class="icon-puzzle"></i>
+                    </div>
+                    <div class="title text-uppercase">
+                        <h4>massive shortcode</h4>
+                    </div>
+                    <div class="desc">
+                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
